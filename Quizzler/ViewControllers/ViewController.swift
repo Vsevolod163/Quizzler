02f920f7d168412ac727a63e463b3c2cb528682f
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-14 14:03:42 +0300

    Labels of buttons added

--- Quizzler/ViewControllers/ViewController.swift
+++ Quizzler/ViewControllers/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var trueButton: UIButton!
+    @IBOutlet var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        trueButton.layer.cornerRadius = 20
+        falseButton.layer.cornerRadius = 20
+        
+        trueButton.layer.borderWidth = 1.0
+        falseButton.layer.borderWidth = 1.0
+        
+        trueButton.layer.borderColor = UIColor.white.cgColor
+        falseButton.layer.borderColor = UIColor.white.cgColor
     }
 
 
